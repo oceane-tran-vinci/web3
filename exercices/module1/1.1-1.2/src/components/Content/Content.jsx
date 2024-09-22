ecbebea6a4a71131ffd5c22bf429367d90e9e97d
oceane.tran <thuyduong.tran@student.vinci.be>
2024-09-22 15:26:24 +0200

    1.1-1.2 : basic components

---
+++ exercices/module1/1.1-1.2/src/components/Content/Content.jsx
@@ -0,0 +1,13 @@
+import Part from "components/Part/Part";
+
+const Content = (props) => {
+    return (
+      <div>
+        <Part name={props.part1} number={props.exercises1} />
+        <Part name={props.part2} number={props.exercises2} />
+        <Part name={props.part3} number={props.exercises3} />
+      </div>
+    );
+  };
+
+export default Content;
