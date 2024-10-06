54b2ba896a2e707914f471c75735802b1e9148a5
oceane.tran <thuyduong.tran@student.vinci.be>
2024-10-06 17:39:19 +0200

    module3 : loading

--- exercices/module3/3.1/src/components/App/App.jsx
+++ exercices/module3/3.1/src/components/App/App.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import Statistics from '../Statistics/Statistics';
 import Button from '../Button/Button';
+import Loading from '../Loading/Loading';
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  //3 : Loading
+  // définir loading sur true au début
+  const [loading, setLoading] = useState(true);
+
+  //set loading to false after 3 seconds
+  setTimeout(() => setLoading(false), 3000);
+
+  //afficher Loading pendant 3 secondes
+  if (loading) return <Loading />;
+
 
   //Event handlers for buttons clicks
     const handleGoodClick = () => setGood(good + 1);
@@ -37,6 +48,29 @@ const App = () => {
     //toFixed(2) formate le nombre avec exactement deux chiffres après la virgule. 
     //pas obliger de mettre (voir dans ex 1.7)
   );
+
+  /* autre solution (prof)
+  const handleClick = (e) => {
+    if (e.target.className === 'good') setGood(good + 1);
+    else if (e.target.className === 'neutral') setNeutral(neutral + 1);
+    else setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <Button onClick={handleClick} value="good" />
+
+      <Button onClick={handleClick} value="neutral" />
+
+      <Button onClick={handleClick} value="bad" />
+
+      <h1>statistics</h1>
+      <Statistics {...{ good, neutral, bad }} />
+    </div>
+  );
+  */
 };
 
 export default App;
--- exercices/module3/3.1/src/components/Statistics/Statistics.jsx
+++ exercices/module3/3.1/src/components/Statistics/Statistics.jsx
@@ -6,6 +6,17 @@ const Statistics = ({ good, neutral, bad, total, average, positivePercentage }) 
     return <p>No feedback given</p>;
   }
 
+  /* autre solution (prof)
+  const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+
+  if (all === 0) return <p>No feedback given</p>;
+
+  const average = all !== 0 ? (good - bad) / all : 0;
+  const positive = all !== 0 ? (good / all) * 100 : 0;
+
+  return () : idem que le prof
+  */
   return (
     <div>
       <h2>Statistics</h2>
